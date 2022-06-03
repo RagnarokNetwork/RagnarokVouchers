5d388ad442382cb6bddee4e4d899286c2cfba870
Zone-Infinity <sohamdokhale1602@gmail.com>
2022-06-03 15:50:36 +0530

    remove debug statements

--- src/main/java/net/ragnaroknetwork/vouchers/event/VoucherUseEvent.java
+++ src/main/java/net/ragnaroknetwork/vouchers/event/VoucherUseEvent.java
@@ -13,23 +13,18 @@ public class VoucherUseEvent implements Listener {
 
     @EventHandler
     public void onPlayerUseVoucher(PlayerInteractEvent event) {
-        System.out.println("Player Interact Event triggered");
         Action action = event.getAction();
         if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) {
-            System.out.println("Right Click Action took place");
             Player player = event.getPlayer();
 
             ItemStack itemInHand = event.getItem();
 
             if (itemInHand == null || itemInHand.getType() == Material.AIR)
                 return;
-            System.out.println("Item in hand is not null or air");
 
             RItemStack item = RItemStack.of(itemInHand);
             if (item.isVoucher()) {
-                System.out.println("Item in hand is voucher");
                 player.performCommand("rvouchers use " + item.getVoucherId());
-                System.out.println(item.getVoucherId() + "voucher used");
             }
         }
     }
